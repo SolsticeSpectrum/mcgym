8e8b7343b87ea0731f6d49d1ee342b29d10f1dbd
SolsticeSpectrum <blackcreepercz012345@gmail.com>
2026-05-29 19:21:57 +0200

    feat(schema): add block/item id registry loader

---
+++ trainer/tests/test_registry.py
@@ -0,0 +1,25 @@
+import pathlib
+
+import pytest
+
+from mcai_train.schema import registry
+
+SAMPLE = pathlib.Path(__file__).resolve().parents[2] / "schema" / "registry.sample.json"
+
+
+def test_load_and_lookup_roundtrip():
+    reg = registry.Registry.load(SAMPLE)
+    assert reg.name_of(reg.id_of("minecraft:oak_log")) == "minecraft:oak_log"
+    assert reg.id_of("minecraft:air") == 0
+    assert reg.size >= 3
+
+
+def test_missing_file_raises():
+    with pytest.raises(FileNotFoundError):
+        registry.Registry.load(pathlib.Path("/no/such/registry.json"))
+
+
+def test_unknown_name_raises():
+    reg = registry.Registry.load(SAMPLE)
+    with pytest.raises(KeyError):
+        reg.id_of("minecraft:does_not_exist")
